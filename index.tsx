ee6fbaa7b0fddfe30c975103706c150c0d402467
Sainath Ganesh <sainathganesh1@gmail.com>
2024-03-23 20:01:15 -0400

    Export Image Component

--- index.tsx
+++ index.tsx
@@ -11,6 +11,7 @@ export { default as Text } from './interactables/Text';
 export { default as Button } from './interactables/Button';
 export { default as Icon } from './interactables/Icon';
 export { default as Input } from './interactables/Input';
+export { default as Image } from './interactables/Image';
 
 // Constants
 export { default as Colors } from './Colors';

b1bd1cd26006e57bad4079e6a38991afdaca5639
Sainath Ganesh <sainathganesh1@gmail.com>
2024-03-24 21:21:53 -0400

    Avatar fix

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,23 +1,24 @@
 import React from "react";
-import { ImageProps } from "react-native";
+import { ImageURISource } from "react-native";
 import Box from "../layout/Box";
 import Image from "../primitives/Image";
 import Text from "../primitives/Text";
 import Colors from "../Colors";
 
 interface Props {
+    source?: ImageURISource;
     alt?: string;
     size?: number;
     color?: keyof typeof Colors;
 }
 
-export default function Avatar({ size = 50, color = 'primary', alt }: Props)
+export default function Avatar({ source, size = 50, color = 'primary', alt }: Props)
 {
     return <Box style={{
         width: size, height: size, borderRadius: size,
         backgroundColor: Colors[color],
     }}>
         <Text style={{ fontSize: 14, fontWeight: '400', color: Colors.white }}>{alt}</Text>
-        <Image resizeMode="center" style={{ position: 'absolute', width: '100%', height: '100%' }} />
+        <Image resizeMode="center" source={source} style={{ position: 'absolute', width: '100%', height: '100%' }} />
     </Box>;
 }

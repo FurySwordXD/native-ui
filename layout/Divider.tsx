6a30a34307c4a2328b2adb7746c6b031595e2ae9
Sainath Ganesh <sainathganesh1@gmail.com>
2024-03-26 15:44:05 -0400

    Divider update

--- layout/Divider.tsx
+++ layout/Divider.tsx
@@ -4,5 +4,5 @@ import Colors from "../Colors";
 
 export default function Divider()
 {
-    return <View style={{ height: 1, backgroundColor: `${Colors.grey}75`, marginTop: 5 }} />;
+    return <View style={{ height: 1, backgroundColor: `${Colors.grey}75`, marginTop: 5, marginBottom: 15 }} />;
 }

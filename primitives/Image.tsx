2808585a95923f0e6e2d92689eee924d3175d644
Sainath Ganesh <sainathganesh1@gmail.com>
2024-03-26 12:06:49 -0400

    Update image props

--- primitives/Image.tsx
+++ primitives/Image.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Image as DefaultImage, ImageProps } from "react-native";
+import { Image as DefaultImage, ImageProps, ImageStyle } from "react-native";
 
-export default function Image({ ...props }: ImageProps)
+interface Props extends ImageProps {
+    size?: number;
+    style?: ImageStyle;
+}
+
+export default function Image({ size, style, ...props }: Props)
 {
-    return <DefaultImage {...props} />;
+    return <DefaultImage {...props} style={{ width: size, height: size, ...style }} />;
 }

7eff526dfb88f81680672e3dc29c1224b7c18076
Sainath Ganesh <sainathganesh1@gmail.com>
2024-03-26 14:28:09 -0400

    Modal change

--- layout/Modal.tsx
+++ layout/Modal.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Modal as DefaultModal, ModalProps, Pressable } from "react-native";
+import { Modal as DefaultModal, ModalProps, ViewStyle, Pressable } from "react-native";
 import View from "./View";
 
 interface Props extends ModalProps
 {
     backdrop?: boolean;
+    style?: ViewStyle;
 }
 
-export default function Modal({ visible, backdrop = true, animationType = 'fade', transparent = true, hardwareAccelerated = true, children, ...props }: Props)
+export default function Modal({ visible, backdrop = true, style, animationType = 'fade', transparent = true, hardwareAccelerated = true, children, ...props }: Props)
 {
     return <DefaultModal
         visible={visible}
@@ -16,9 +17,11 @@ export default function Modal({ visible, backdrop = true, animationType = 'fade'
         animationType={animationType}
         {...props}
     >
-        <Pressable onPress={props.onRequestClose} style={{ position: 'absolute', width: '100%', height: '100%', backgroundColor: 'transparent' }}>
-            <View style={{ width: '100%', height: '100%', backgroundColor: visible && backdrop && 'rgba(0, 0, 0, 0.5)' }} />
-        </Pressable>
-        {children}
+        <View style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', ...style }}>
+            <Pressable onPress={props.onRequestClose} style={{ position: 'absolute', width: '100%', height: '100%', backgroundColor: 'transparent' }}>
+                <View style={{ width: '100%', height: '100%', backgroundColor: visible && backdrop && 'rgba(0, 0, 0, 0.5)' }} />
+            </Pressable>
+            {children}
+        </View>
     </DefaultModal>;
 }

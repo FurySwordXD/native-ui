e2dd981c4606bbddcce96fff8d0d92ae650361a4
Sainath Ganesh <sainathganesh1@gmail.com>
2024-03-27 00:28:15 -0400

    Support more Icon types

--- primitives/Icon.tsx
+++ primitives/Icon.tsx
@@ -4,11 +4,16 @@ import AntDesign from "react-native-vector-icons/AntDesign";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import MCIcon from "react-native-vector-icons/MaterialCommunityIcons";
 import Fontisto from "react-native-vector-icons/Fontisto";
+
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+
 import Colors from "../Colors";
 
 interface Props  {
     name: string;
-    type?: 'AntDesign' | 'Fontisto' | 'Material' | 'MaterialCommunity';
+    type?: 'AntDesign' | 'Fontisto' | 'Material' | 'MaterialCommunity' | 'Entypo' | 'FontAwesome5' ;
     size?: number;
     color?: string;
 }
@@ -24,6 +29,10 @@ export default function Icon({ name, size = 24, color = Colors.primary, type = '
             return <MCIcon name={name} size={size} color={color} />;
         case "Fontisto":
             return <Fontisto name={name} size={size} color={color} />;
+        case "FontAwesome5":
+            return <FontAwesome5 name={name} size={size} color={color} />;
+        case "Entypo":
+            return <Entypo name={name} size={size} color={color} />;
         default:
             return <MCIcon name={name} size={size} color={color} />;
     }
